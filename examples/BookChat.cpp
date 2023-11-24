3f440d3e98a731bb636ddcc7bee249e8073d700f
David-xF <davidxf174@gmail.com>
2023-11-24 18:52:19 +0100

    Removed Stuff from BookChat

--- examples/BookChat.cpp
+++ examples/BookChat.cpp
@@ -40,20 +40,16 @@ DECL_FUNCTION(void, ServerPlayer_tick, mc::ServerPlayer* player) {
         item->tag->getString(strTitle, L"title");
         item->tag->getString(strAuthor, L"author");
 
-        item->setTag(new mc::CompoundTag());
-        item->item = mc::Item::byId(386);
-
         wchar_t temp[0x40];
-        mc_swprintf(temp, 0x40, L"%s%ls: %ls", player->isModerator() ? "[Moderator] " : "[Not Moderator] ", strAuthor.c_str(),  strTitle.c_str());
+        mc_swprintf(temp, 0x40, L"%s%ls: %ls", player->isModerator() ? "[Moderator] " : "", strAuthor.c_str(),  strTitle.c_str());
 
         for (mc_boost::shared_ptr<mc::ServerPlayer>& _player : mc::MinecraftServer::getInstance()->getPlayers()->players) {
             _player->listener->send(new mc::ClientboundChatPacket(temp));
             _player->listener->send(new mc::ClientboundSoundPacket(mc::SoundEvent::note_hat));
         }
                 
-        int isEmpty = item->getCount() == 0;
-        if (item->item != mc::Item::byId(386) || isEmpty) {
-            mc::ItemInstance* chat = new mc::ItemInstance(mc::Item::byId(386), 1);
+        if (item->item != mc::Item::byId(386)) {
+            mc::ItemInstance* chat = new mc::ItemInstance(mc::Item::byId(386), item->getCount());
             xf::ItemInstanceHelper::setName(chat, L"Book Chat");
             item->item = mc::Item::byId(0);
             item->count = 0;
