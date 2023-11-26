acaaca35ca3cf13464e7933a605772a35fce96c0
David-xF <davidxf174@gmail.com>
2023-11-26 11:33:19 +0100

    Added handleSound to ClientPacketListener.h

--- include/minecraft/network/listener/ClientPacketListener.h
+++ include/minecraft/network/listener/ClientPacketListener.h
@@ -14,5 +14,9 @@ namespace mc {
 		void sendCommand(uint32_t commandAddr = 0x10303000) {
 	    	code::Func<void, 0x0304A5D8, ClientPacketListener*, uint32_t>()(this, commandAddr);
 		}
+
+		void handleSoundEvent(const mc_boost::shared_ptr<struct ClientboundSoundPacket>& packet) {
+			code::Func<void, 0x0306EB2C, ClientPacketListener*, const mc_boost::shared_ptr<struct ClientboundSoundPacket>&>()(this, packet);
+    	}
 	};
 }
